2c0ed8de708ac3f369fc71074bf1edbe1b229519
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2023-06-07 04:04:38 +0100

    Fix bugs with commas rendering at the top of table

--- main.js
+++ main.js
@@ -49,6 +49,6 @@ function writeToDocument(url) {
             });
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
